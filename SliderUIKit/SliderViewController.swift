af4b380ac5edcb9b1f9be0857c48de36b4f8c45a
id2k1149 <id2k1149@gmail.com>
2023-04-28 11:09:29 -0400

    refactor

--- SliderUIKit/SliderViewController.swift
+++ SliderUIKit/SliderViewController.swift
@@ -9,36 +9,22 @@ import UIKit
 
 class SliderViewController: UIViewController {
 
-    @IBOutlet var sliderValue: UILabel!
     @IBOutlet var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSlider()
-        setupLabel()
     }
     
     @IBAction func sliderAction() {
-        sliderValue.text = "\(Int(slider.value))"
         let alphaComponent = 1 - slider.value / 100
         slider.thumbTintColor = UIColor.red.withAlphaComponent(CGFloat(alphaComponent))
-    }
-    
-    private func setupLabel() {
-        sliderValue.text = "0"
-        sliderValue.font = UIFont.systemFont(ofSize: 32)
-        sliderValue.textAlignment = .center
     }
     
     private func setupSlider() {
         slider.value = 0
         slider.minimumValue = 0
         slider.maximumValue = 100
-        slider.minimumTrackTintColor = .green
-        slider.maximumTrackTintColor = .blue
         slider.thumbTintColor = UIColor.red.withAlphaComponent(1)
     }
-
-
 }
-
