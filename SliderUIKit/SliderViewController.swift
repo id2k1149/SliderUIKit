57c9981f3c25c917660ca47292f9dc35738c71bb
id2k1149 <id2k1149@gmail.com>
2023-04-28 08:51:10 -0400

    Initial Commit

---
+++ SliderUIKit/SliderViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SliderUIKit
+//
+//  Created by Max Franz Immelmann on 4/28/23.
+//
+
+import UIKit
+
+class SliderViewController: UIViewController {
+
+    @IBOutlet var sliderValue: UILabel!
+    @IBOutlet var slider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
